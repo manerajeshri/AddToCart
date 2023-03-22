f82c1c56e8261b914617f629b76e151a20e27895
manerajeshri <manerajeshri@gmail.com>
2023-03-22 23:05:10 +0530

    imported FormsModule,     ReactiveFormsModule
    imported FormsModule,
    ReactiveFormsModule

--- AddToCart/src/app/app.module.ts
+++ AddToCart/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
