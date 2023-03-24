d6975b1b65e549e0e109d1426e88c69000ece399
manerajeshri <manerajeshri@gmail.com>
2023-03-24 23:47:17 +0530

    login form
    login form

--- AddToCart/src/app/app.module.ts
+++ AddToCart/src/app/app.module.ts
@@ -6,6 +6,31 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// adding modules given by sir
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import {  MatInputModule } from "@angular/material/input";
+import {  MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import {  MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -15,8 +40,33 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // Adding Required Modules From Angular Material
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+ MatSliderModule,
+ MatMenuModule,
+ MatButtonModule,
+ MatIconModule,
+ MatCardModule,
+ MatTabsModule,
+ MatSidenavModule,
+ MatListModule,
+ MatToolbarModule,
+ MatInputModule,
+ MatTableModule,
+ MatPaginatorModule,
+ MatSortModule,
+ MatProgressSpinnerModule,
+ MatDialogModule,
+ MatSelectModule,
+ MatDatepickerModule,
+ MatNativeDateModule,
+ MatRadioModule,
+ MatCheckboxModule,
+ MatStepperModule,
+ MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
